6a04fa338f87c94039b3402ca0430a8f3afda7cf
chingaloEric <echingalo@gmail.com>
2020-10-27 10:44:39 +0300

    feat: 🎸 read the json object from the file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import * as object from './constants/object.json';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'hisp-interview-question';
+
+  constructor() {
+    console.log(object['default']);
+  }
 }
